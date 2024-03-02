db6b6ed53ce1523b6c9efcbb42a0ff50a82c8cf1
Ellie <eagyeman1234@gmail.com>
2024-03-02 13:48:18 -0500

    create function to keep track of players score

--- project1.py
+++ project1.py
@@ -24,4 +24,22 @@ while True:
 max_score = 50
 player_scores =[0 for _ in range(players)]
 
-print(player_scores)
+while max(player_scores) < max_score:
+
+  for player_idx in range(players):
+    current_score = 0
+
+    while True:
+      should_roll = input('Would you like to roll (y)?')
+      if should_roll.lower() != 'y':
+        break
+      
+      value = roll()
+      if value  == 1:
+        print('You rolled a 1! Turn done!')
+        break
+      else:
+        current_score += value
+        print('You rolled a: ', value)
+      
+      print('Your score is: ', current_score)
