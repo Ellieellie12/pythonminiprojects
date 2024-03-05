ff88066c247663659125ca15cbb071483900c365
Ellie <eagyeman1234@gmail.com>
2024-03-05 13:45:05 -0500

    add eval function to get the answers to the random problems

--- project3.py
+++ project3.py
@@ -10,7 +10,9 @@ def generate_problem():
   operator = random.choice(OPERATORS)
 
   expr = str(left) + ' ' + operator + ' ' + str(right)
+  answer = eval(expr)
   print(expr)
-  return(expr)
+  return expr , answer
 
-generate_problem()
+expr, answer = generate_problem()
+print(expr, answer)
